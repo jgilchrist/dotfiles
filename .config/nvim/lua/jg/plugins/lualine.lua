41277445ba66d163479a69960b308ed66744acde
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-04-06 00:38:14 +0100

    nvim: Enable icons for lualine

--- .config/nvim/lua/jg/plugins/lualine.lua
+++ .config/nvim/lua/jg/plugins/lualine.lua
@@ -1,7 +1,6 @@
 require'lualine'.setup({
   options = {
     theme = 'auto',
-    icons_enabled = false,
     component_separators = { left='', right='' },
     section_separators = { left='', right='' },
   },
