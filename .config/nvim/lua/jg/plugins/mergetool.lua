a60bc401b485ffebcb346a1d7cabd530ae7fa6bc
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-04-05 21:10:57 +0100

    nvim: Pass lua callbacks to plugins

--- .config/nvim/lua/jg/plugins/mergetool.lua
+++ .config/nvim/lua/jg/plugins/mergetool.lua
@@ -1,22 +1,20 @@
 vim.g.mergetool_layout = 'ml,b'
 vim.g.mergetool_prefer_revision = 'remote'
 
--- TODO: Find a way to pass OnMergetoolSetLayout as a lua function
-vim.cmd [[
-function! OnMergetoolSetLayout(split)
-  lua vim.wo.cursorline = false
-  lua require'jg.config'.disable_cursorline_follows_focus()
-  lua vim.keymap.set('n', '<leader>mt', ':MergetoolToggle<cr>')
+function on_mergetool_set_layout(split)
+  vim.wo.cursorline = false
+  require'jg.config'.disable_cursorline_follows_focus()
+  vim.keymap.set('n', '<leader>mt', ':MergetoolToggle<cr>')
 
-  " When base is horizontal split at the bottom
-  " Turn off diff mode, and show syntax highlighting
-  " Also let it take less height
-  if (a:split['layout'] ==# 'mr,b' || a:split['layout'] ==# 'ml,b') && a:split['split'] ==# 'b'
-    lua vim.wo.diff = false
-    lua vim.bo.syntax = 'OFF'
-    resize 15
-  endif
-endfunction
+  -- When base is horizontal split at the bottom
+  -- Turn off diff mode, and show syntax highlighting
+  -- Also let it take less height
+  vim.pretty_print(split)
+  if (split['layout'] == 'mr,b' or split['layout'] == 'ml,b') and split['split'] == 'b' then
+    vim.wo.diff = false
+    vim.bo.syntax = 'OFF'
+    vim.cmd [[resize 15]]
+  end
+end
 
-let g:MergetoolSetLayoutCallback = function('OnMergetoolSetLayout')
-]]
+vim.g.MergetoolSetLayoutCallback = on_mergetool_set_layout
--- .config/nvim/lua/jg/plugins/wiki.lua
+++ .config/nvim/lua/jg/plugins/wiki.lua
@@ -8,21 +8,11 @@ vim.g.wiki_link_target_type = 'md'
 vim.g.wiki_filetypes = {'md'}
 vim.g.wiki_tags_format_pattern = '\\v%(^|\\s)#\\zs[^# ]+'
 
--- TODO: Find a way to pass WikiMapCreatePage and WikiMapLinkCreate as lua functions
-vim.cmd[[
-function! WikiMapCreatePage(text) abort
-  return substitute(tolower(a:text), '\s\+', '-', 'g')
-endfunction
+function snakecase(text)
+  return text:lower():gsub('%s+', '-')
+end
 
-let g:wiki_map_create_page = 'WikiMapCreatePage'
-]]
-
-vim.cmd[[
-function! WikiMapLinkCreate(text) abort
-  return substitute(tolower(a:text), '\s\+', '-', 'g')
-endfunction
-
-let g:wiki_map_link_create = 'WikiMapLinkCreate'
-]]
+vim.g.wiki_map_create_page = snakecase
+vim.g.wiki_map_link_create = snakecase
 
 vim.keymap.set('n', '<leader>ww', ':WikiFzfPages<CR>')
