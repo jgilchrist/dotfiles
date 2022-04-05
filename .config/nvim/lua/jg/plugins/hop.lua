13a5bc4ca165c78e0657377c59c7e57491271b13
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-04-05 23:35:35 +0100

    nvim: Add new hop.nvim mappings that don't conflict
    These previously conflicted with vim-surround, removing the ability to
    use hop motions in operator-pending mode.

--- .config/nvim/lua/jg/plugins/hop.lua
+++ .config/nvim/lua/jg/plugins/hop.lua
@@ -2,7 +2,6 @@ require'hop'.setup()
 
 vim.keymap.set('n', 's', '<nop>')
 
-vim.keymap.set({'n', 'o'}, 'sw', ':HopWord<cr>')
-vim.keymap.set({'n', 'o'}, 'sl', ':HopLineStart<cr>')
-vim.keymap.set({'n', 'o'}, 'sc', ':HopChar1<cr>')
-vim.keymap.set({'n', 'o'}, 'ss', ':HopChar2<cr>')
+vim.keymap.set({'n', 'o'}, 'gt', ':HopChar1<CR>')
+vim.keymap.set({'n', 'o'}, 'ga', ':HopAnywhereMW<CR>')
+vim.keymap.set({'n', 'o'}, 'gw', ':HopWord<CR>')
