5d8b5d22322b17f5654a56d401f2414892b04280
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-04-07 20:48:06 +0100

    nvim: Use string formatting in plugin_config to avoid extra fn defs

--- .config/nvim/lua/jg/plugins.lua
+++ .config/nvim/lua/jg/plugins.lua
@@ -11,7 +11,7 @@ end
 bootstrap_packer()
 
 function plugin_config(n)
-  require('jg.plugins.'..n)
+  return string.format([[require 'jg.plugins.%s']], n)
 end
 
 augroup('packer_autocompile', function(autocmd)
@@ -22,14 +22,14 @@ packer.startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = function() plugin_config('treesitter') end}
+  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = plugin_config('treesitter')}
 
   -- Extensions to vim's language
   use 'tpope/vim-repeat'
-  use {'numToStr/Comment.nvim', config = function() plugin_config('comment') end }
+  use {'numToStr/Comment.nvim', config = plugin_config('comment')}
   use 'tpope/vim-surround'
   use 'wellle/targets.vim'
-  use {'phaazon/hop.nvim', config = function() plugin_config('hop') end }
+  use {'phaazon/hop.nvim', config = plugin_config('hop')}
   use 'tommcdo/vim-exchange'
 
   use 'kana/vim-textobj-user'
@@ -39,29 +39,29 @@ packer.startup(function(use)
 
   -- File management
   use {'junegunn/fzf', run = ':call fzf#install()' }
-  use {'junegunn/fzf.vim', config = function() plugin_config('fzf') end}
+  use {'junegunn/fzf.vim', config = plugin_config('fzf')}
 
-  use {'justinmk/vim-dirvish', config = function() plugin_config('dirvish') end}
+  use {'justinmk/vim-dirvish', config = plugin_config('dirvish')}
   use 'tpope/vim-eunuch'
 
   use 'tpope/vim-unimpaired'
   use {'tpope/vim-fugitive', cmd = {'Git'}}
 
   -- Colorscheme
-  use {'projekt0n/github-nvim-theme', config = function() plugin_config('github') end}
+  use {'projekt0n/github-nvim-theme', config = plugin_config('github')}
 
   -- Extras
   use {'nvim-lualine/lualine.nvim',
     after = 'github-nvim-theme',
-    config = function() plugin_config('lualine') end
+    config = plugin_config('lualine')
   }
 
   use {'junegunn/goyo.vim', cmd = {'Goyo'}}
   use 'editorconfig/editorconfig-vim'
-  use {'jgilchrist/vim-mergetool', config = function() plugin_config('mergetool') end, cmd = { 'MergetoolStart', 'MergetoolToggle' } }
+  use {'jgilchrist/vim-mergetool', config = plugin_config('mergetool'), cmd = { 'MergetoolStart', 'MergetoolToggle' } }
 
   use {'lervag/wiki.vim',
-    config = function() plugin_config('wiki') end,
+    config = plugin_config('wiki'),
     cmd = 'WikiFzfPages',
     setup = function()
       vim.keymap.set('n', '<leader>ww', ':WikiFzfPages<CR>')
@@ -70,7 +70,7 @@ packer.startup(function(use)
 
   -- Experiments
   use {'dhruvasagar/vim-table-mode', cmd = { 'TableModeToggle' }}
-  use {'ekickx/clipboard-image.nvim', config = function() plugin_config('clipboardimage') end, ft = { 'markdown' }}
+  use {'ekickx/clipboard-image.nvim', config = plugin_config('clipboardimage'), ft = { 'markdown' }}
   -- use 'simrat39/rust-tools.nvim'
   -- use 'neovim/nvim-lspconfig'
 
