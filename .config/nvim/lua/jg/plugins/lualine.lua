49e3e5cc4b24a6daf2c9809372dda8c6fe9c2fac
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2023-08-09 13:04:18 +0100

    nvim: Remove more unnecessary lualine components

--- .config/nvim/lua/jg/plugins/lualine.lua
+++ .config/nvim/lua/jg/plugins/lualine.lua
@@ -1,11 +1,15 @@
 require'lualine'.setup({
   options = {
     theme = 'auto',
-    component_separators = { left='', right='' },
-    section_separators = { left='', right='' },
+    component_separators = '',
+    section_separators = '',
   },
   sections = {
     -- Remove diff/diagnostics info section
     lualine_b = {},
+    -- Remove encoding and file format/type section
+    lualine_x = {},
+    -- Remove progress % section
+    lualine_y = {''},
   },
 })
