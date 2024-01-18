1ce88877cad57a769e8daad019ce083e5517ec95
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2024-01-18 12:11:46 +0000

    nvim: Remove zk code from after/markdown.lua

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,11 +1,1 @@
 require'jg.util'.use_text_mode()
-
-if require("zk.util").notebook_root(vim.fn.expand('%:p')) ~= nil then
-  vim.wo.conceallevel=2
-
-  -- <CR> opens links in normal mode
-  vim.api.nvim_buf_set_keymap(0, "n", "<CR>", "<Cmd>lua vim.lsp.buf.definition()<CR>", { noremap=true, silent=false })
-
-  -- <CR> creates a new page in select mode
-  vim.api.nvim_buf_set_keymap(0, 'x', '<CR>', ':ZkNewFromTitleSelection { edit = false }<CR>', { noremap=true, silent=false })
-end
