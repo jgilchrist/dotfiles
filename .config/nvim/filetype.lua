85e86c8e6b2d0e6965a7cfa1fdcc8abbfbcc525f
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2024-08-13 14:20:10 +0100

    nvim: Class Directory.Packages.props as XML

--- .config/nvim/filetype.lua
+++ .config/nvim/filetype.lua
@@ -2,6 +2,7 @@ vim.filetype.add({
   filename = {
     ['Directory.Build.props'] = 'xml',
     ['Directory.Build.targets'] = 'xml',
+    ['Directory.Packages.props'] = 'xml',
     ['Justfile'] = 'make',
   },
 })
