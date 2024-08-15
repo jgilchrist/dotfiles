bca4a428df32c86fd68f266e10009e54cc139091
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2024-08-15 17:02:28 +0100

    wezterm: Disable ligatures

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,5 +6,7 @@ config.color_scheme = 'Tomorrow Night Bright'
 
 config.window_decorations = 'RESIZE'
 
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+
 localconfig.apply_to_config(config)
 return config
