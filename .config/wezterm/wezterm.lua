06af29dda5b20aa9ec30ff7e683d496ff27175ae
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2024-08-15 17:03:25 +0100

    wezterm: Switch to previous color scheme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,34 @@ local wezterm = require 'wezterm'
 local localconfig = require 'local'
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Tomorrow Night Bright'
+config.colors = {
+  foreground = '#E6EDF3',
+  background = '#010409',
+
+  selection_fg = '#264F78',
+  selection_bg = '#E6EDF3',
+
+  ansi = {
+    '#484F59',
+    '#F37B72',
+    '#3FB850',
+    '#E1D330',
+    '#58A6FF',
+    '#BC8CFF',
+    '#4BC4CF',
+    '#B1BAC3',
+  },
+  brights = {
+    '#6E7680',
+    '#F15F50',
+    '#56D364',
+    '#E3B341',
+    '#79C0FF',
+    '#D2A8FF',
+    '#56D4DD',
+    '#FFFFFF',
+  },
+}
 
 config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
 
