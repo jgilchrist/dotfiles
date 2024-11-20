3fd143f0478975556fe436f87b4a0702927c4713
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2024-11-20 11:52:03 +0000

    fish: Add an alias to allow '.....' to cd up to parent directories

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -1,3 +1,8 @@
+function multicd
+    echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
+end
+abbr --add dotdot --regex '^\.\.+$' --function multicd
+
 # /usr/bin aliases
 alias mv="mv --interactive --verbose"
 alias cp="cp --interactive --verbose"
