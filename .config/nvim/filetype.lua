04dcbb9bb8ea2ea0816d7a4014e5e989e1e4ac98
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2025-03-26 23:04:42 +0000

    nvim: Remove filetype.lua
    All filetypes here are now covered by default in 0.11.

--- .config/nvim/filetype.lua
+++
@@ -1,8 +0,0 @@
-vim.filetype.add({
-  filename = {
-    ['Directory.Build.props'] = 'xml',
-    ['Directory.Build.targets'] = 'xml',
-    ['Directory.Packages.props'] = 'xml',
-    ['Justfile'] = 'make',
-  },
-})
