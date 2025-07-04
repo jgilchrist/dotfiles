92f7609ceeaa6a042a08afa8ae50e2e174d5d168
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2025-07-04 11:02:04 +0100

    nvim: Switch to vim.pack for plugin management
    For my own small set of plugins, I'm not relying on anything from Lazy
    that can't be replicated by nvim 0.12's new plugin management system.

--- .config/nvim/lua/jg/util.lua
+++ .config/nvim/lua/jg/util.lua
@@ -1,21 +1,5 @@
 local M = {}
 
-function M.bootstrap_lazynvim()
-  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-  if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable", -- latest stable release
-      lazypath,
-    })
-  end
-
-  vim.opt.rtp:prepend(lazypath)
-end
-
 function M.reload_config()
   for name,_ in pairs(package.loaded) do
     -- Work round the fact that these modules would otherwise be loaded from the cache
