8f27a220a0fd4323f50d277621bbbbcece70c815
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2026-04-07 12:20:31 +0100

    nvim: Set more ui2 options

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,12 +11,60 @@ require'jg.disable_builtins'
 require'jg.plugins'.setup()
 require'jg.lang'.setup()
 
-require'vim._core.ui2'.enable({})
-
 -- Settings {{{
 
 local util = require 'jg.util'
 local augroup = util.augroup
+
+-- UI2
+vim.o.cmdheight = 0
+require("vim._core.ui2").enable({
+  enable = true,
+  msg = {
+    targets = {
+      [""] = "msg",
+      empty = "cmd",
+      bufwrite = "msg",
+      confirm = "cmd",
+      emsg = "pager",
+      echo = "msg",
+      echomsg = "msg",
+      echoerr = "pager",
+      completion = "cmd",
+      list_cmd = "pager",
+      lua_error = "pager",
+      lua_print = "msg",
+      progress = "pager",
+      rpc_error = "pager",
+      quickfix = "msg",
+      search_cmd = "cmd",
+      search_count = "cmd",
+      shell_cmd = "pager",
+      shell_err = "pager",
+      shell_out = "pager",
+      shell_ret = "msg",
+      undo = "msg",
+      verbose = "pager",
+      wildlist = "cmd",
+      wmsg = "msg",
+      typed_cmd = "cmd",
+    },
+    cmd = {
+      height = 0.5,
+    },
+    dialog = {
+      height = 0.5,
+    },
+    msg = {
+      height = 0.3,
+      timeout = 5000,
+    },
+    pager = {
+      height = 0.5,
+    },
+  },
+})
+
 
 -- Don't show the intro message when starting Vim
 vim.opt.shortmess:append('I')
